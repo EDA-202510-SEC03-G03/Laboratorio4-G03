7e18be533ea88f06f47b8844c8de2524b85c2d7f
sramosr2 <s.ramosr2@uniandes.edu.co>
2025-02-13 10:34:00 -0500

    funciones

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -1,1 +1,31 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def peek(my_queue): 
+    pass
+    
+def is_empty(my_queue):
+    if my_queue["size"] == 0:
+        return True 
+    else:
+        return False 
+
+def size(my_queue):
+    return my_queue["size"]
