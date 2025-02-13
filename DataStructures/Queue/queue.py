0bcd4e767718b0d3ab36e4dfd36ad1dd3b348f7f
SLaraU <s.larau@uniandes.edu.co>
2025-02-13 10:40:29 -0500

    Enqueue y Dequeue

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -9,6 +9,17 @@ def new_queue():
 
 
 
+def enqueue(my_queue: a, element):
+    my_queue.add_last(element)
+    return my_queue
+    
+def dequeue(my_queue: a):
+    if a.is_empty(my_queue):
+        raise Exception('EmptyStructureError: queue is empty')
+    
+    elemento_a_eliminar = my_queue.first_element()
+    my_queue.remove_first()
+    return elemento_a_eliminar
 
 
 
